08222ed28b5ba30698a44ba6ebfcfacd520d0bea
Kev <kcjtoft@gmail.com>
2020-10-20 17:43:14 -0400

    removed dependency on haused jsonPlaceHolder api for data

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 import './App.css';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}))
     }
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-            <div className='tc'>
-                <h1 className='f1'>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-
-            </div>
-        );
+        if (this.state.robots.length ===0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
 
     onSearchChange = (event) => {
